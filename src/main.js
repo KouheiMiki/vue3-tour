624b8e3d314675bcb9bb5dd0619b26cbad5c33dd
KouheiMiki <kouhei.miki0926@gmail.com>
2023-03-01 23:32:08 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue3Tour from 'vue3-tour'
 
-createApp(App).mount('#app')
+import 'vue3-tour/dist/vue3-tour.css'
+
+const app = createApp(App)
+app.use(Vue3Tour)
+app.mount('#app')
